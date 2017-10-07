63ae6d97cbb6f1ef3875a9cfa45abeb6cdcc56a7
Alexander Dimitrov <a.dimitrov93@gmail.com>
2017-10-07 17:59:24 +0300

    Created endpoint for controlling and getting status of heaters

--- WebService/WebService.go
+++ WebService/WebService.go
@@ -2,15 +2,19 @@ package WebService
 
 import (
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
+	"strconv"
 
+	"github.com/gaeshi-gaeshi/thermo-smart/HeatersController"
 	"github.com/gaeshi-gaeshi/thermo-smart/TemperatureSensorController"
 )
 
 // Start is used to setup and start the web service
 func Start() {
 	http.HandleFunc("/temperature", temperatureHandler)
+	http.HandleFunc("/heaters", heatersHandler)
 	http.ListenAndServe(":9999", nil)
 }
 
@@ -24,5 +28,28 @@ func temperatureHandler(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprint(w, currentTemperature)
 	} else if r.Method == "POST" {
 		// Change target temperature
+	} else {
+		w.WriteHeader(http.StatusNotFound)
+	}
+}
+
+func heatersHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "GET" {
+		fmt.Fprint(w, HeatersController.GetNumberOfWorkingHeaters())
+	} else if r.Method == "POST" {
+		bodyBytes, error := ioutil.ReadAll(r.Body)
+		if error != nil {
+			log.Fatal(error)
+		}
+
+		bodyString := string(bodyBytes)
+		numberOfHeaters, error := strconv.Atoi(bodyString)
+		if error != nil {
+			log.Fatal(error)
+		}
+
+		HeatersController.SetNumberOfWorkingHeaters(numberOfHeaters)
+	} else {
+		w.WriteHeader(http.StatusNotFound)
 	}
 }
